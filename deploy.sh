80a550d4a2fd6bf36b624fe24edf4e58f75f1a4f
Murali Boppudi <muraliboppudi8@gmail.com>
2026-04-18 15:56:52 +0530

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+echo "🚀 Starting Deployment..."
 
-echo " Starting Deployment..."
+# Pull latest image
+docker pull boppudimurali/lone-mlops:latest
 
-# Pull the latest Docker image from DockerHub
-docker pull your_dockerhub_username/lone-mlops:latest
-
-# Stop and remove old container if running
-docker stop iris-app 2>/dev/null || true
-docker rm iris-app 2>/dev/null || true
+# Stop & remove old container
+docker stop lone-app 2>/dev/null || true
+docker rm lone-app 2>/dev/null || true
 
 # Run new container
 docker run -d \
   --name lone-app \
   -p 8000:8000 \
   --restart always \
-  your_dockerhub_username/lone-mlops:latest
+  boppudimurali/lone-mlops:latest
 
-echo " Deployment Complete! App running on port 8000"
+echo "✅ Deployment Complete! App running on port 8000"
